ae9286146563bc3927dd1418c186b21cc6c84854
komsunpa <ssijee@gmail.com>
2026-08-21 17:39:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "jp-brick-game-v2";
-const ASSETS = [
+const CACHE_NAME="jp-brick-game-v5";
+const ASSETS=[
   "./",
   "./index.html",
   "./manifest.webmanifest",
@@ -8,31 +8,23 @@ const ASSETS = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", event => {
+self.addEventListener("activate",event=>{
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./index.html"));
-    })
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
   );
 });
